7a1f20c8d0f0fcee22b3a52ce11926b61ef258b2
Christopher Joslyn <christopher.joslyn@gmail.com>
2017-05-23 16:59:13 -0400

    WIP Add a quick example d3 chart
    TODOs:
    
    - Avoid importing everything from d3 at once
    - Move renderChart to a component

--- src/views/home/home.controller.js
+++ src/views/home/home.controller.js
@@ -1,7 +1,27 @@
+import * as d3 from 'd3';
+import './home.scss';
+
 export default class HomeController {
   constructor (primaryUser) {
     'ngInject';
 
     this.name = `${primaryUser.firstName} ${primaryUser.lastName}`;
+
+    this.renderChart();
+  }
+  renderChart () {
+    const data = [4, 8, 15, 16, 23, 42];
+
+    const container = d3.select('.chart')
+      .selectAll('div')
+      .data(data);
+
+    const scale = d3.scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
+
+    container.enter().append('div')
+      .style('width', d => scale(d) + 'px')
+      .text(d => d);
   }
 }
